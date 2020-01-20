9d508bc37ad3a32146610049493a6b61cff10a63
felipegontijo <epilef.gontijo@gmail.com>
2020-01-20 15:25:32 -0600

    Connect server-side code to client-side code

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const app = express();
 const bodyParser = require('body-parser');
 
 /* Middleware */
-
 // use body-parser as middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
 // Cors for cross origin allowance
 app.use(cors());
+
+
+// Initialize main project folder
+app.use(express.static('website'));

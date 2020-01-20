9c7e01a62919d6ebc7c4ce971691260f7def7cdb
felipegontijo <epilef.gontijo@gmail.com>
2020-01-20 15:22:48 -0600

    Configure 'body-parser' as middleware

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 // Use Express
 const express = require('express');
 const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
+
+/* Middleware */
+
+// use body-parser as middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());

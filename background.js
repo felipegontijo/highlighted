1cd22a597a525ab4fcc37959b70023d76fcea475
felipegontijo <epilef.gontijo@gmail.com>
2020-02-04 18:11:21 -0600

    feat: Get text selected by the user

---
+++ background.js
@@ -0,0 +1,21 @@
+// get text selected by the user
+function getSelectionText() {
+    let text = "";
+    let activeEl = document.activeElement;
+    let activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
+    if (
+      (activeElTagName == "textarea") || (activeElTagName == "input" &&
+      /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
+      (typeof activeEl.selectionStart == "number")
+    ) {
+        text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
+    } else if (window.getSelection) {
+        text = window.getSelection().toString();
+    }
+    return text;
+}
+
+// writes text selected into text box -- testing purposes
+// document.onmouseup = document.onkeyup = document.onselectionchange = () => {
+//   document.getElementById("sel").value = getSelectionText();
+// };

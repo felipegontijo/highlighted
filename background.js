0ac481d6ddc77dace466b4456a1b6bed245991cc
felipegontijo <epilef.gontijo@gmail.com>
2020-02-11 12:40:18 -0600

    refactor: Remove alert before executing content script
    Alert before executing content script was to test the function.
    No need to have it for now.

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 /* execute content script when extension clicked */
 chrome.browserAction.onClicked.addListener(() => {
-    alert('Executing content script...');
     chrome.tabs.executeScript({file: 'content.js'});
 });

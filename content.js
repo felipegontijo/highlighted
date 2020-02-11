f6a3de6e11cd71ef2204137c818d8aeb5a0ae01e
felipegontijo <epilef.gontijo@gmail.com>
2020-02-11 12:55:41 -0600

    feat: Get user selection

--- content.js
+++ content.js
@@ -1,21 +1,9 @@
-// get text selected by the user
+/* get user selection */
 function getSelectionText() {
-    let text = "";
-    let activeEl = document.activeElement;
-    let activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
-    if (
-      (activeElTagName == "textarea") || (activeElTagName == "input" &&
-      /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
-      (typeof activeEl.selectionStart == "number")
-    ) {
-        text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
-    } else if (window.getSelection) {
-        text = window.getSelection().toString();
+    if (window.getSelection) {
+        return window.getSelection();
     }
-    return text;
 }
 
-// alerts text selected -- testing purposes
-document.onmouseup = document.onkeyup = document.onselectionchange = () => {
-    alert(getSelectionText());
-  };
+/* alert user selection -- testing purposes */
+alert(getSelectionText());

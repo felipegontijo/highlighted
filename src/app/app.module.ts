0873021138998b7dea30c549073f5e2a9c778586
felipegontijo <epilef.gontijo@gmail.com>
2020-02-19 16:34:23 -0600

    feat: Create Hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Header } from './header/header.component';
+import { Hero } from './hero/hero.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    Header
+    Header,
+    Hero
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'hero',
+    templateUrl: './hero.component.html',
+    styleUrls: ['./hero.component.css']
+})
+
+export class Hero {
+
+}

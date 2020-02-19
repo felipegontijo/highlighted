67132a6e1767f99eaba70abf3825d5ee6cb05ec7
felipegontijo <epilef.gontijo@gmail.com>
2020-02-19 17:45:35 -0600

    feat: Create teaser component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Header } from './header/header.component';
 import { Hero } from './hero/hero.component'
+import { Teaser } from './teaser/teaser.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Header,
-    Hero
+    Hero,
+    Teaser
   ],
   imports: [
     BrowserModule,
---
+++ src/app/teaser/teaser.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'teaser',
+    templateUrl: './teaser.component.html',
+    styleUrls: ['./teaser.component.css']
+})
+
+export class Teaser {
+
+}

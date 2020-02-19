bab68ccdcf175c6549d5c8eb7023be7793455283
felipegontijo <epilef.gontijo@gmail.com>
2020-02-19 09:37:29 -0600

    feat: Import and declare header component in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Header } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Header
   ],
   imports: [
     BrowserModule,

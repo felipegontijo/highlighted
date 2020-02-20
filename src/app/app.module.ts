03ccc432e94b5bd2f624275982176091d2a9575d
felipegontijo <epilef.gontijo@gmail.com>
2020-02-20 10:51:03 -0600

    style: Separate imports according to their nature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component'
 import { TeaserComponent } from './teaser/teaser.component';

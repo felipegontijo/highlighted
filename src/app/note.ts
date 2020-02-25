61bebb0b68fef270550936f896c75cf646ee089f
felipegontijo <epilef.gontijo@gmail.com>
2020-02-25 10:13:22 -0600

    feat: Create Note interface

---
+++ src/app/note.ts
@@ -0,0 +1,5 @@
+export interface Note {
+    url: string;
+    title: string;
+    content: string;
+}
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from '../note';
 
 @Component({
   selector: 'app-notes',

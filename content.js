ed2ca40af1fd2fdf02e3f0269cb36c5c4bd95380
felipegontijo <epilef.gontijo@gmail.com>
2020-02-26 15:58:54 -0600

    feat: Add template post function

--- content.js
+++ content.js
@@ -5,5 +5,26 @@ function getUserSelection() {
     }
 }
 
-/* testing purposes */
-alert(getUserSelection());
+function getTabTitle() {
+    const tabUrl = '';
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        tabUrl = tabs[0].url;     //url
+    });
+    return tabUrl;
+}
+
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log('error', error);
+    }
+}

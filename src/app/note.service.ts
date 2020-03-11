778569db9fa15609e3fbecd99e1112603c174000
felipegontijo <epilef.gontijo@gmail.com>
2020-03-11 17:46:12 -0500

    feat: Add error handling to http get

--- src/app/note.service.ts
+++ src/app/note.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Note } from './note';
 import { NOTES } from './mock-notes';
 import { Observable, of } from 'rxjs';
@@ -12,8 +13,33 @@ export class NoteService {
   private notesUrl = 'http://localhost:3000/api/notes';
 
   getNotes(): Observable<Note[]> {
-    return this.http.get<Note[]>(this.notesUrl);
+    return this.http.get<Note[]>(this.notesUrl)
+      .pipe(
+        catchError(this.handleError<Note[]>('getNotes', []))
+      )
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * From angular docs.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
   constructor(private http: HttpClient) { }
 }
+
